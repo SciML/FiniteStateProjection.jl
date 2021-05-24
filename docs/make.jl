0064a0a188bf93d5d779f2db40eab7effc78d9da
kaandocal <26488673+kaandocal@users.noreply.github.com>
2021-05-24 11:42:12 +0100

    Fixed master/main branch confusion in Documentation workflow

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,4 @@ using Documenter, FiniteStateProjection
 
 makedocs(sitename="FiniteStateProjection.jl", format = Documenter.HTML(prettyurls = false))
 
-deploydocs(repo = "github.com/kaandocal/FiniteStateProjection.jl.git")
+deploydocs(repo = "github.com/kaandocal/FiniteStateProjection.jl.git", devbranch="main")
