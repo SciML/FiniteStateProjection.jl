e11c735c35f0d69bd4b925da89184e8766dfc4a0
kaandocal <26488673+kaandocal@users.noreply.github.com>
2021-05-24 11:34:44 +0100

    Fixing documentation workflow

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,5 @@
 using Documenter, FiniteStateProjection
 
 makedocs(sitename="FiniteStateProjection.jl", format = Documenter.HTML(prettyurls = false))
+
+deploydocs(repo = "github.com/kaandocal/FiniteStateProjection.jl.git")
