27f760b2d88f97e2f0e0ec276353c19ca8d307b8
kaandocal <26488673+kaandocal@users.noreply.github.com>
2023-09-25 14:45:47 +0100

    Optimised tests somewhat

--- test/birthdeath2D.jl
+++ test/birthdeath2D.jl
@@ -22,14 +22,14 @@ prs = exp.(2 .* rand(2))
 ps = [ prs[1], prs[1] / exp(4 * rand()),
        prs[2], prs[2] / exp(4 * rand()) ]
 
-Nmax = 130
+Nmax = 100
 u0 = zeros(Nmax+1, Nmax+1)
 u0[1] = 1.0
 
 tt = [ 0.25, 1.0, 10.0 ]
 
 prob = convert(ODEProblem, sys, u0, 10.0, ps)
-sol = solve(prob, Vern7(), abstol=1e-9, reltol=1e-6, saveat=tt)
+sol = solve(prob, Vern7(), abstol=1e-6, saveat=tt)
 
 @test marg(sol.u[1], dims=2) ≈ pdf.(Poisson(ps[1] / ps[2] * (1 - exp(-ps[2] * tt[1]))), 0:Nmax) atol=1e-4
 @test marg(sol.u[1], dims=1) ≈ pdf.(Poisson(ps[3] / ps[4] * (1 - exp(-ps[4] * tt[1]))), 0:Nmax) atol=1e-4
@@ -44,7 +44,7 @@ A = convert(SparseMatrixCSC, sys, (Nmax+1, Nmax+1), ps, 0)
 f = (du,u,t) -> mul!(du, A, u)
 
 probA = ODEProblem(f, u0, 10.0)
-solA = solve(prob, Vern7(), abstol=1e-9, reltol=1e-6, saveat=tt)
+solA = solve(prob, Vern7(), abstol=1e-6, saveat=tt)
 
 @test sol.u[1] ≈ solA.u[1] atol=1e-4
 @test sol.u[2] ≈ solA.u[2] atol=1e-4
