8987d707360da76f952dc5a1fbe44dcade732369
kaandocal <26488673+kaandocal@users.noreply.github.com>
2024-06-25 16:27:08 -0600

    Moved faster test to the front

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using SafeTestsets
 
 @time begin
-    @safetestset "BirthDeath2D" begin include("birthdeath2D.jl") end
     @safetestset "FeedbackLoop" begin include("feedbackloop.jl") end
+    @safetestset "BirthDeath2D" begin include("birthdeath2D.jl") end
 end
